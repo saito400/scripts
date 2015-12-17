1040252c7e68c9b8a3f88eaf677258d66290b4de
saito400 <ken.s.530000@gmail.com>
2015-12-17 10:05:07 +0900

    add http

---
+++ ruby/http_json.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require 'erb'
+require 'net/http'
+require 'json'
+
+url = 'https://api.spotify.com/v1/search?type=artist&q='
+param = 'maroon 5'
+
+uri = URI(url + ERB::Util.url_encode(param))
+
+response = Net::HTTP.get(uri)
+json = JSON.parse(response)
+
+p json
